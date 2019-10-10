7bcd2bbc9454e6a2f023970282bc42166084cd3b
Anton <antonymartynov@gmail.com>
2019-10-11 02:52:28 +0300

    Changed BaseExecutor.execute comment

--- executors/base.py
+++ executors/base.py
@@ -4,8 +4,10 @@ class BaseExecutor(metaclass=ABCMeta):
     """ Abstract executor interface """
     def execute(self, command, parameters=None):
         """
-        Initiates system call
+        Initiates command execution
 
+        :command: string, command to execute
+        :parameters: tuple, params for command
         :return: result code, stdout, stderr
         """
         raise NotImplementedError
