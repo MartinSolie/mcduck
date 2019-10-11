68718ca7ff427977a1c4c7bdb0a8ca7ff2b8a23f
Anton <antonymartynov@gmail.com>
2019-10-11 03:01:20 +0300

    Updated LocalExecutor
    Added encoding param into initialization

--- executors/local.py
+++ executors/local.py
@@ -3,8 +3,23 @@ import subprocess
 from executors.base import BaseExecutor
 
 class LocalExecutor(BaseExecutor):
-    """ Will make system calls locally """
+    """Creates subprocess and executes command locally"""
+    DEFAULT_ENCODING = 'utf-8'
+
+    def __init__(self, encoding=DEFAULT_ENCODING):
+        self.encoding = encoding
+
     def execute(self, command, parameters=None):
-        parameters = parameters or []
-        res = subprocess.run([command] + list(parameters), capture_output=True)
-        return res.returncode, res.stdout.decode('utf-8'), res.stderr.decode('utf-8')
+        """Initiates command execution
+
+        :command: string, command to execute
+        :parameters: tuple, params for command
+        :return: result code, stdout, stderr
+        """
+        if parameters:
+            command = [command] + list(parameters)
+
+        res = subprocess.run(command, capture_output=True)
+        return (res.returncode,
+                res.stdout.decode(self.encoding),
+                res.stderr.decode(self.encoding))
