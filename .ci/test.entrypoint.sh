42385b59b76103eeff550089bd443f20150b0d0e
Anton <antonymartynov@gmail.com>
2019-10-15 08:07:53 +0300

    Updated xinetd restart waiting method

--- .ci/test.entrypoint.sh
+++ .ci/test.entrypoint.sh
@@ -1,8 +1,7 @@
 /usr/sbin/sshd \
 && /etc/init.d/xinetd restart \
-&& sleep 2 \
+&& until /etc/init.d/xinetd status > /dev/null 2>&1; do sleep 1; done \
 && python3 -m unittest \
     tests.test_ssh_executor \
     tests.test_telnet_executor \
     tests.test_local_executor
-
